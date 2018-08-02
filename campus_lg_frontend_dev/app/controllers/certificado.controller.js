86437f34d4487a34039ae6421f7df204934f4e0f
unknown <enzojbnss@gmail.com>
2018-08-02 12:26:39 -0300

    exportação de pdf

--- campus_lg_frontend_dev/app/controllers/certificado.controller.js
+++ campus_lg_frontend_dev/app/controllers/certificado.controller.js
@@ -1,6 +1,21 @@
 LGApp.controller("CertificadoController", function ($scope, $rootScope, $cookies, $location, $timeout) {
         $timeout(function () {
             $rootScope.userAuthWithSessionId(function (userResponse) {})
+            
+            html2canvas(document.body, {
+                onrendered: function(canvas) {
+                    // document.body.appendChild(canvas);
+    				var data = canvas.toDataURL();
+                    var docDefinition = {
+                        content: [{
+                            image: data,
+                            width: 600,
+    						pageOrientation: 'landscape'
+                        }]
+                    };
+                    pdfMake.createPdf(docDefinition).open();
+                }
+            });
         }, 10)
     }
 );
