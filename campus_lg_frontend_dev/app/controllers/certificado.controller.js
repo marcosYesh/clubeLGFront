9390d30e0318de51de1cf95697d7816dac0cb89b
unknown <enzojbnss@gmail.com>
2018-08-02 15:43:26 -0300

    teste user response

--- campus_lg_frontend_dev/app/controllers/certificado.controller.js
+++ campus_lg_frontend_dev/app/controllers/certificado.controller.js
@@ -1,22 +1,23 @@
 LGApp.controller("CertificadoController", function ($scope, $rootScope, $cookies, $location, $timeout) {
         $timeout(function () {
-            $rootScope.userAuthWithSessionId(function (userResponse) {})
-            
-            html2canvas(document.getElementById('ContentPrint'), {
-                onrendered: function(canvas) {
-                    // document.body.appendChild(canvas);
-    				var data = canvas.toDataURL();
-                    var docDefinition = {
-                    	pageOrientation: 'landscape',	
-                        content: [{
-                            image: data,
-                            width: 600
-    						
-                        }]
-                    };
-                    pdfMake.createPdf(docDefinition).download();
-                }
-            });
+            $rootScope.userAuthWithSessionId(function (userResponse) {
+            	html2canvas(document.getElementById('ContentPrint'), {
+                    onrendered: function(canvas) {
+                        // document.body.appendChild(canvas);
+        				var data = canvas.toDataURL();
+                        var docDefinition = {
+                        	pageOrientation: 'landscape',	
+                            content: [{
+                                image: data,
+                                width: 600
+        						
+                            }]
+                        };
+                        pdfMake.createPdf(docDefinition).download();
+                    }
+                });	
+            	
+            })            
         }, 10)
     }
 );
