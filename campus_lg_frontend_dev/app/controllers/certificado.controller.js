25b97f69f01af2de73645d08d2ff319ef25073ce
unknown <enzojbnss@gmail.com>
2018-08-02 14:48:43 -0300

    codificação cretificado controller

--- campus_lg_frontend_dev/app/controllers/certificado.controller.js
+++ campus_lg_frontend_dev/app/controllers/certificado.controller.js
@@ -5,25 +5,13 @@ LGApp.controller("CertificadoController", function ($scope, $rootScope, $cookies
             html2canvas(document.body, {
                 onrendered: function(canvas) {
                     // document.body.appendChild(canvas);
-    				var data = "Olá mundo!";
+    				var data = canvas.toDataURL();
                     var docDefinition = {
-                    		content: [
-                    		          // if you don't need styles, you can use a simple string to define a paragraph
-                    		          'This is a standard paragraph, using default style',
-
-                    		          // using a { text: '...' } object lets you set styling properties
-                    		          { text: 'This paragraph will have a bigger font', fontSize: 15 },
-
-                    		          // if you set pass an array instead of a string, you'll be able
-                    		          // to style any fragment individually
-                    		          {
-                    		            text: [
-                    		              'This paragraph is defined as an array of elements to make it possible to ',
-                    		              { text: 'restyle part of it and make it bigger ', fontSize: 15 },
-                    		              'than the rest.'
-                    		            ]
-                    		          }
-                    		        ]
+                        content: [{
+                            image: data,
+                            width: 600,
+    						pageOrientation: 'landscape'
+                        }]
                     };
                     pdfMake.createPdf(docDefinition).open();
                 }
