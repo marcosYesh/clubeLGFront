7229745bb7f4d5c7f22fc458d914e143fe93d3a6
unknown <enzojbnss@gmail.com>
2018-08-03 11:46:47 -0300

    implementação do service

--- campus_lg_frontend_dev/app/controllers/certificado.controller.js
+++ campus_lg_frontend_dev/app/controllers/certificado.controller.js
@@ -1,33 +1,26 @@
-LGApp.controller("CertificadoController", function ($scope, $rootScope, $cookies, $location, $timeout,apiService) {
-	    $scope.nome = "";
-	
-	    $timeout(function () {
-            $rootScope.userAuthWithSessionId(function (userResponse) {
-            	apiService.authenticate(1, userResponse.Token).then(function (response) {
-            		$scope.nome = response.data.ReturnData.Name;  
-            		html2canvas(document.getElementById('ContentPrint'), {
-                        onrendered: function(canvas) {
-                            // document.body.appendChild(canvas);
-            				var data = canvas.toDataURL();
-                            var docDefinition = {
-                            	pageOrientation: 'landscape',	
-                                content: [{
-                                    image: data,
-                                    width: 600
-            						
-                                }]
-                            };
-                            pdfMake.createPdf(docDefinition).download();
-                        }
-                    });	
-                });
-            	
-            	
-            	
-            	
-            	
-            	
-            })            
-        }, 10)
-    }
-);
+LGApp.controller("CertificadoController", function($scope, $rootScope, $cookies, $location, $timeout, video, apiService, $sce, $http, $interval, $routeParams, $route) {
+	$scope.nome = "";
+
+	$timeout(function() {
+		$rootScope.userAuthWithSessionId(function(userResponse) {
+			apiService.authenticate(userResponse.id, userResponse.Token).then(
+					function(response) {
+						$scope.nome = response.data.ReturnData.Name;
+						html2canvas(document.getElementById('ContentPrint'), {
+							onrendered : function(canvas) {
+								// document.body.appendChild(canvas);
+								var data = canvas.toDataURL();
+								var docDefinition = {
+									pageOrientation : 'landscape',
+									content : [ {
+										image : data,
+										width : 600
+									} ]
+								};
+								pdfMake.createPdf(docDefinition).download();
+							}
+						});
+					});
+		})
+	}, 10)
+});
