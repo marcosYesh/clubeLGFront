da603e5a4524106249309ee556c964fde1117c4b
unknown <enzojbnss@gmail.com>
2018-08-03 12:08:03 -0300

    implementação do service

--- campus_lg_frontend_dev/app/controllers/certificado.controller.js
+++ campus_lg_frontend_dev/app/controllers/certificado.controller.js
@@ -1,26 +1,25 @@
-LGApp.controller("CertificadoController", function($scope, $rootScope, $cookies, $location, $timeout, video, apiService, $sce, $http, $interval, $routeParams, $route) {
+LGApp.controller("CertificadoController", function($scope, $rootScope,
+		$cookies, $location, $timeout, video, apiService, $sce, $http,
+		$interval, $routeParams, $route) {
 	$scope.nome = "";
 
 	$timeout(function() {
 		$rootScope.userAuthWithSessionId(function(userResponse) {
-			apiService.authenticate(userResponse.id, userResponse.Token).then(
-					function(response) {
-						$scope.nome = response.data.ReturnData.Name;
-						html2canvas(document.getElementById('ContentPrint'), {
-							onrendered : function(canvas) {
-								// document.body.appendChild(canvas);
-								var data = canvas.toDataURL();
-								var docDefinition = {
-									pageOrientation : 'landscape',
-									content : [ {
-										image : data,
-										width : 600
-									} ]
-								};
-								pdfMake.createPdf(docDefinition).download();
-							}
-						});
-					});
+			$scope.nome = userResponse.Name;
+			html2canvas(document.getElementById('ContentPrint'), {
+				onrendered : function(canvas) {
+					// document.body.appendChild(canvas);
+					var data = canvas.toDataURL();
+					var docDefinition = {
+						pageOrientation : 'landscape',
+						content : [ {
+							image : data,
+							width : 600
+						} ]
+					};
+					pdfMake.createPdf(docDefinition).download();
+				}
+			});
 		})
 	}, 10)
 });
