2865f0910d414faeeb517a9fe9b7ba8721299061
unknown <enzojbnss@gmail.com>
2018-08-03 11:20:42 -0300

    implementação do service

--- campus_lg_frontend_dev/app/controllers/certificado.controller.js
+++ campus_lg_frontend_dev/app/controllers/certificado.controller.js
@@ -4,27 +4,28 @@ LGApp.controller("CertificadoController", function ($scope, $rootScope, $cookies
 	    $timeout(function () {
             $rootScope.userAuthWithSessionId(function (userResponse) {
             	apiService.authenticate(1, userResponse.Token).then(function (response) {
-            		$scope.nome = response.data.ReturnData.Name;            		
+            		$scope.nome = response.data.ReturnData.Name;  
+            		html2canvas(document.getElementById('ContentPrint'), {
+                        onrendered: function(canvas) {
+                            // document.body.appendChild(canvas);
+            				var data = canvas.toDataURL();
+                            var docDefinition = {
+                            	pageOrientation: 'landscape',	
+                                content: [{
+                                    image: data,
+                                    width: 600
+            						
+                                }]
+                            };
+                            pdfMake.createPdf(docDefinition).download();
+                        }
+                    });	
                 });
             	
             	
             	
             	
-            	html2canvas(document.getElementById('ContentPrint'), {
-                    onrendered: function(canvas) {
-                        // document.body.appendChild(canvas);
-        				var data = canvas.toDataURL();
-                        var docDefinition = {
-                        	pageOrientation: 'landscape',	
-                            content: [{
-                                image: data,
-                                width: 600
-        						
-                            }]
-                        };
-                        pdfMake.createPdf(docDefinition).download();
-                    }
-                });	
+            	
             	
             })            
         }, 10)
